c47b3f8b14439d17893d8978250eab6341f56e7d
Jorge-Ceron <a01278485@tec.mx>
2026-08-12 08:28:08 -0600

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/Drive/DriveConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.Drive;
+
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants;
+import frc.robot.Robot;
+
+public class DriveConstants {
+    
+}
